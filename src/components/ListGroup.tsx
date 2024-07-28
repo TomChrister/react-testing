d5bc15914f7b2cad0c1f426dca8b8780805d234b
Tom Christer Schoon <142898062+TomChrister@users.noreply.github.com>
2024-07-28 17:32:39 +0200

    added prettier

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,30 @@
 function ListGroup() {
-    const items = ["New York", "Oslo", "Stockholm", "Copenhagen", "Santiago"];
+  const items = [
+    "New York",
+    "Oslo",
+    "Stockholm",
+    "Copenhagen",
+    "Santiago",
+    "Tokyo",
+  ];
 
-    return (
-        <>
-            <h1>List</h1>
-            {items.length === 0 && <p>No item found</p>}
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li className="list-group-item" key={item}>{item}</li>
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
